2144787efccc6c4be3c5ad9ee5dd0113977a3255
Akira Vernon <akirav@hawaii.edu>
2018-02-07 16:58:42 -1000

    Two buttons

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      count1: 0
     }
 }
 
 handleClick(){
   this.setState(prevState => {
     return {count: prevState.count + 1}
+  });
+}
+
+handleClick2(){
+  this.setState(prevState => {
+    return {count1: prevState.count1 + 1}
   });
 }
 
@@ -25,12 +32,17 @@ handleClick(){
         <p className="App-intro">
         </p>
 
+
         <button onClick={this.handleClick.bind(this)}>Button 1 </button>
+        <button onClick={this.handleClick2.bind(this)}>Button 2 </button>
 
         <div>
           Button 1: {this.state.count} clicks
         </div>
 
+        <div>
+          Button 2: {this.state.count1} clicks
+        </div>
 
 
       </div>
