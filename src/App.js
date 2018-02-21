3e7bd7ef59eb6ce2724d42aba8896b8fc3140639
Akira Vernon <akirav@hawaii.edu>
2018-02-21 13:51:41 -1000

    Finish working on the Image Buttons

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ class App extends Component {
       h2: "The entire range of wavelengths of electromagnetic radiation.",
       h3: "Cable Company",
       h4: "Kyle Chan's Company",
+      p1: "https://static.shoplightspeed.com/shops/609235/files/002231373/80x80x1/new-life-spectrum-spectrum-small-fish-50g-5mm.jpg",
+      p2:"http://spectrum-photonics.com/wp-content/uploads/2013/06/SPECTRUM-LOGOS.gif",
+      p3: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq0A2DyxaUSJkHHeV5KHNbuFzXumuNlkmnIYv7EtSbBF2VCl3I",
+      p4: "http://spectrum-miami.com/wp-content/uploads/2016/06/SMIA16-Preloader.png",
       display: "Choose a Hint above",
+      display2: "https://vignette.wikia.nocookie.net/uncyclopedia/images/0/01/DramaticQuestionMark.png/revision/latest/scale-to-width-down/180?cb=20060419021703"
     }
 }
 
@@ -49,6 +54,30 @@ handlehint3(){
 handlehint4(){
   this.setState(prevState => {
     return {display: this.state.h4}
+  });
+}
+
+handlepic1(){
+  this.setState(prevState => {
+    return {display2: this.state.p1}
+  });
+}
+
+handlepic2(){
+  this.setState(prevState => {
+    return {display2: this.state.p2}
+  });
+}
+
+handlepic3(){
+  this.setState(prevState => {
+    return {display2: this.state.p3}
+  });
+}
+
+handlepic4(){
+  this.setState(prevState => {
+    return {display2: this.state.p4}
   });
 }
 
@@ -89,6 +118,16 @@ handlehint4(){
           {this.state.display}.
         </header>
 
+        <h1 className ="Big">Part 3: Guess the word using pictures? 🤔</h1>
+
+          <div>
+            <button onClick={this.handlepic1.bind(this)}>Pic 1</button>
+            <button onClick={this.handlepic2.bind(this)}>Pic 2</button>
+            <button onClick={this.handlepic3.bind(this)}>Pic 3</button>
+            <button onClick={this.handlepic4.bind(this)}>Pic 4</button>
+          </div>
+
+          <div class="Box"><img src={this.state.display2} alt="img"/></div>
 
       </div>
 
